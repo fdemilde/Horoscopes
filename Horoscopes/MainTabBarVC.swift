ea9b0318a5c854796690ac30004bc9a68b0ed8eb
Binh Dang <magician0811@gmail.com>
2015-06-03 15:01:05 +0700

    init

---
+++ Horoscopes/MainTabBarVC.swift
@@ -0,0 +1,9 @@
+//
+//  MainTabBarVC.swift
+//  Horoscopes
+//
+//  Created by Binh Dang on 6/3/15.
+//  Copyright (c) 2015 Binh Dang. All rights reserved.
+//
+
+import Foundation
---
+++ Horoscopes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Horoscopes
+//
+//  Created by Binh Dang on 6/3/15.
+//  Copyright (c) 2015 Binh Dang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
