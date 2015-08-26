cd3f1a3e8ec0e3cc84259bd985df5e883dd2fcb3
Dang Doan <dangdoan@floatingcube.com>
2015-08-26 12:33:13 +0700

    Add shadow to profile's image and change minor details.

--- Horoscopes/ViewController/Newsfeed/NewsfeedTableViewCell.swift
+++ Horoscopes/ViewController/Newsfeed/NewsfeedTableViewCell.swift
@@ -11,12 +11,21 @@ import UIKit
 class NewsfeedTableViewCell: UITableViewCell {
 
     @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var horoscopeSignView: UIView!
+    @IBOutlet weak var profileImageView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        containerView.layer.cornerRadius = 5
+        containerView.layer.cornerRadius = 4
         containerView.clipsToBounds = true
+        horoscopeSignView.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.2)
+        horoscopeSignView.layer.cornerRadius = 4
+        horoscopeSignView.clipsToBounds = true
+        profileImageView.layer.shadowOffset = CGSize(width: 0, height: 3)
+        profileImageView.layer.shadowOpacity = 0.5
+        profileImageView.layer.shadowRadius = 1
+        profileImageView.clipsToBounds = false
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
