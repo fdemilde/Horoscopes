710f668ec2204d2c5e97e374a0965efb4d54e82c
Dang Doan <dangdoan@floatingcube.com>
2015-09-17 17:11:45 +0700

    Delete unnecessary data in DataStore.

--- Horoscopes/DataStore.swift
+++ Horoscopes/DataStore.swift
@@ -12,27 +12,7 @@ class DataStore : NSObject{
     var newsfeedGlobal = [UserPost]()
     var newsfeedFollowing = [UserPost]()
     var newsfeedIsUpdated : Bool = false
-    var userPosts = [UserPost]() {
-        didSet {
-            if isDataUpdated(oldValue, newData: userPosts) {
-                NSNotificationCenter.defaultCenter().postNotificationName(NOTIFICATION_UPDATE_POST, object: userPosts)
-            }
-        }
-    }
-    var followers = [UserProfile]() {
-        didSet {
-            if isDataUpdated(oldValue, newData: followers) {
-                NSNotificationCenter.defaultCenter().postNotificationName(NOTIFICATION_UPDATE_FOLLOWERS, object: followers)
-            }
-        }
-    }
-    var followingUsers = [UserProfile]() {
-        didSet {
-            if isDataUpdated(oldValue, newData: followingUsers) {
-                NSNotificationCenter.defaultCenter().postNotificationName(NOTIFICATION_UPDATE_FOLLOWING, object: followingUsers)
-            }
-        }
-    }
+    
     var recentSearchedProfile = [UserProfile]()
     var currentUserProfile: UserProfile?
     var isLastPage = false
@@ -166,23 +146,6 @@ class DataStore : NSObject{
     }
     
     // MARK: - Helpers
-    func isDataUpdated<T: SequenceType>(oldData: T, newData: T) -> Bool {
-        let oldDataIdSet = getDataIds(oldData)
-        let newDataIdSet = getDataIds(newData)
-        return oldDataIdSet != newDataIdSet
-    }
-    
-    func getDataIds<T: SequenceType>(data: T) -> Set<String> {
-        var result = Set<String>()
-        for item in data {
-            if let post = item as? UserPost {
-                result.insert(post.post_id)
-            } else if let profile = item as? UserProfile {
-                result.insert("\(profile.uid)")
-            }
-        }
-        return result
-    }
     
     func compareAndUpdateArrayData(oldDataArray : [UserPost], newDataArray : [UserPost]) -> [UserPost]{
         var removedArray = [UserPost]()
