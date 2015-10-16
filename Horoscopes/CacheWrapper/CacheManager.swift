e6eefb21aecd14e1909ce52259e901fa5678d844
Dang Doan <dangdoan@floatingcube.com>
2015-10-16 16:27:06 +0700

    Invalidate the cache when refresh user's feed.

--- Horoscopes/CacheWrapper/CacheManager.swift
+++ Horoscopes/CacheWrapper/CacheManager.swift
@@ -16,7 +16,7 @@ class CacheManager {
     class func cacheGet(url : String, postData : NSMutableDictionary?, loginRequired: LoginReq, expiredTime : NSTimeInterval, forceExpiredKey: String?, completionHandler: (result: NSDictionary?, error: NSError?) -> Void){
         dispatch_async(dispatch_get_main_queue()) { () -> Void in
             let key = Utilities.getKeyFromUrlAndPostData(url, postData: postData)
-            //        print("cacheGet key == \(key)")
+//                    print("cacheGet key == \(key)")
             let cacheDict = NSUserDefaults.standardUserDefaults().dictionaryForKey(key)
             //        print("cacheGet cacheDict == \(cacheDict)")
             if var cacheDict = cacheDict{
@@ -24,11 +24,11 @@ class CacheManager {
                 let cacheValue = cacheDict["CACHE_VALUE_KEY"] as! NSDictionary
                 let cacheExpiredTime = cacheDict["CACHE_EXPIRED_TIMESTAMP_KEY"] as! String
                 if(NSDate().timeIntervalSince1970 < Double(cacheExpiredTime)){ // valid
-                    //                print("GOT CACHE !! RETURN")
+                                    print("GOT CACHE !! RETURN")
                     completionHandler(result: cacheValue, error: nil) // return cache
                     return
                 }
-                //            print("GOT CACHE BUT EXPIRED")
+                            print("GOT CACHE BUT EXPIRED")
                 completionHandler(result: cacheValue, error: nil) // return expired cache but still call to server
             }
             Utilities.showHUD()
@@ -37,18 +37,18 @@ class CacheManager {
                     completionHandler(result: nil, error: error)
                 } else {
                     //                print("SERVER DATA == \(response)")
-                if let forceExpiredKey = forceExpiredKey {
-                    // use for supporting page, need to force next page expired if current page is expired
-                    if(forceExpiredKey != "") { CacheManager.cacheExpire(forceExpiredKey) }
-                    
-                }
-                
-                if let errorRes = response["error"]{
-                    if(errorRes as! Int == 0){
-                        CacheManager.cachePut(key, value:response, expiredTime: expiredTime)
+                    if let forceExpiredKey = forceExpiredKey {
+                        // use for supporting page, need to force next page expired if current page is expired
+                        if(forceExpiredKey != "") { CacheManager.cacheExpire(forceExpiredKey) }
+                        
                     }
-                }
-                completionHandler(result: response, error: error)
+                    
+                    if let errorRes = response["error"]{
+                        if(errorRes as! Int == 0){
+                            CacheManager.cachePut(key, value:response, expiredTime: expiredTime)
+                        }
+                    }
+                    completionHandler(result: response, error: error)
                 }
                 Utilities.hideHUD()
             }
@@ -56,7 +56,7 @@ class CacheManager {
     }
     
     class func cachePut(key:String, value: NSObject, expiredTime : NSTimeInterval){
-//        print("cachePut key == \(key)")
+        print("cachePut key == \(key)")
         var cacheDict = Dictionary<String, NSObject>()
         cacheDict["CACHE_VALUE_KEY"] = value
         cacheDict["CACHE_EXPIRED_TIMESTAMP_KEY"] = String(expiredTime)
