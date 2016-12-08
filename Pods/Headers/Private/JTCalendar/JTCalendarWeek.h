f990a82e21706dbc20a62d9fca4e82f5538569ae
Jayven Nhan <nhantrivinh@gmail.com>
2016-12-08 08:46:23 +0700

    committed

--- Pods/Headers/Private/JTCalendar/JTCalendarWeek.h
+++ Pods/Headers/Private/JTCalendar/JTCalendarWeek.h
@@ -1,1 +1,19 @@
-../../../JTCalendar/JTCalendar/Protocols/JTCalendarWeek.h
+//
+//  JTCalendarWeek.h
+//  JTCalendar
+//
+//  Created by Jonathan Tribouharet
+//
+
+#import <Foundation/Foundation.h>
+
+@class JTCalendarManager;
+
+@protocol JTCalendarWeek <NSObject>
+
+- (void)setManager:(JTCalendarManager *)manager;
+
+- (NSDate *)startDate;
+- (void)setStartDate:(NSDate *)startDate updateAnotherMonth:(BOOL)enable monthDate:(NSDate *)monthDate;
+
+@end
--- Pods/Headers/Private/JTCalendar/JTHorizontalCalendarView.h
+++ Pods/Headers/Private/JTCalendar/JTHorizontalCalendarView.h
@@ -1,1 +1,23 @@
-../../../JTCalendar/JTCalendar/Views/JTHorizontalCalendarView.h
+//
+//  JTHorizontalCalendar.h
+//  JTCalendar
+//
+//  Created by Jonathan Tribouharet
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JTContent.h"
+
+@interface JTHorizontalCalendarView : UIScrollView<JTContent>
+
+@property (nonatomic, weak) JTCalendarManager *manager;
+
+@property (nonatomic) NSDate *date;
+
+/*!
+ * Must be call if override the class
+ */
+- (void)commonInit;
+
+@end
